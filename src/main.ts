f95d8d27912ac02dca43ff07bc919017fff24b43
Alireza <alirezazamanidev80@gmail.com>
2024-08-23 23:16:11 +0330

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import './configs/env.config'
+import './configs/env.config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { urlencoded } from 'express';
+import SwaggerConfig from './configs/swagger.config';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // set App configs
-  app.enableCors({origin:"*"});
+  app.enableCors({ origin: '*' });
   app.use(cookieParser());
   app.setGlobalPrefix('/api');
 
-  //  App Run 
-  const {PORT}=process.env
-  await app.listen(PORT,()=>{
+  // Swagger conifg
+  SwaggerConfig(app);
+  //  App Run
+  const { PORT } = process.env;
+  await app.listen(PORT, () => {
     console.log(`Server Run http://localhost:${PORT}/api`);
     console.log(`Swagger Run http://localhost:${PORT}/swagger`);
-    
-  })
+  });
 }
 bootstrap();
